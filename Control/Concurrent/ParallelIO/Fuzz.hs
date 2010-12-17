ee314aa4c01c8a832b97e292e4c969d83ececaae
Max Bolingbroke <batterseapower@hotmail.com>
2010-12-17 08:53:54 +0000

    Verbose output from the fuzer

--- Control/Concurrent/ParallelIO/Fuzz.hs
+++ Control/Concurrent/ParallelIO/Fuzz.hs
@@ -25,8 +25,8 @@ sPAWN_PERCENTAGE = 2
 mAX_WORKERS = 3
 
 
-showFloat2 :: RealFloat a => a -> String
-showFloat2 x = Numeric.showFFloat (Just 2) x ""
+showFloat :: RealFloat a => a -> String
+showFloat x = Numeric.showFFloat (Just 2) x ""
 
 
 expected :: Fractional b => (Int, Int) -> b
@@ -35,16 +35,22 @@ expected (top, bottom) = fromIntegral (top + bottom) / 2
 main :: IO ()
 main = do
     -- Birth rate is the rate at which new work items enter the queue
-    putStrLn $ "Expected birth rate: " ++ showFloat2 ((expected sPAWN_RANGE * (fromIntegral sPAWN_PERCENTAGE / 100) * fromIntegral mAX_WORKERS) / expected dELAY_RANGE :: Double)
+    putStrLn $ "Expected birth rate: " ++ showFloat ((expected sPAWN_RANGE * (fromIntegral sPAWN_PERCENTAGE / 100) * fromIntegral mAX_WORKERS) / (expected dELAY_RANGE / 1000000) :: Double) ++ " items/second"
     -- Service rate is the rate at which work items are removed from the pool
-    putStrLn $ "Expected service rate: " ++ showFloat2 (fromIntegral mAX_WORKERS / expected dELAY_RANGE :: Double)
+    putStrLn $ "Expected service rate: " ++ showFloat (fromIntegral mAX_WORKERS / (expected dELAY_RANGE / 1000000) :: Double) ++ " items/second"
     -- We are balanced on average if birth rate == service rate, i.e. expected sPAWN_RANGE * (fromIntegral sPAWN_PERCENTAGE / 100) == 1
-    putStrLn $ "Balance factor (should be 1): " ++ showFloat2 (expected sPAWN_RANGE * (fromIntegral sPAWN_PERCENTAGE / 100) :: Double)
+    putStrLn $ "Balance factor (should be 1): " ++ showFloat (expected sPAWN_RANGE * (fromIntegral sPAWN_PERCENTAGE / 100) :: Double)
     withPool mAX_WORKERS $ \pool -> forever (fuzz pool)
 
 fuzz pool = do
     n <- randomRIO sPAWN_RANGE
-    parallel_ pool $ replicate n $ do
+    tid <- myThreadId
+    putStrLn $ show tid ++ ":\t" ++ show n
+    parallel_ pool $ flip map [1..n] $ \i -> do
         should_spawn <- fmap (<= sPAWN_PERCENTAGE) $ randomRIO (1, 100)
+        nested_tid <- myThreadId
+        
+        putStrLn $ show tid ++ ":\trunning " ++ show i ++ if should_spawn then " (recursing)" else ""
+        
         randomRIO dELAY_RANGE >>= threadDelay
         when should_spawn $ fuzz pool
